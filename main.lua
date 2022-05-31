9da2961b494e579ef6db42202bbb2926d5394157
Ryan Arana <ryan.arana@hey.com>
2022-05-31 09:00:17 -0700

    Change segLength and numPoints to prepare for using images for the chain
    then fix the bugs that change uncovered 😅 
    
    Turns out having the gravity set so high and the constraintIterations so low, made the chain stretch out real far and I didn't realize it because I couldn't see the whole thing. These changes put the full chain on screen for now to help get the sim working. Later I'll need to move it back up and set its length and boundaries appropriately for the game
    
    This commit also takes care of some garbage collection issues that were causing the game to completely freeze for half a second every 2 or 3 seconds

--- main.lua
+++ main.lua
@@ -11,36 +11,37 @@ function clamp(n, low, high)
 	return math.min(math.max(low, n), high)
 end
 
--- local player = Chain()
-
--- pd.inputHandlers.push({
--- 	cranked = function(change, acceleratedChange)
--- 		player:cranked(change, acceleratedChange)
--- 	end
--- })
-
--- local screenShakeSprite = ScreenShake()
-
-local numPoints = 8
-local segLength = 32
+local numPoints = 10
+local segLength = 24
+local chainLength = numPoints * segLength
 local points = table.create(numPoints, 0)
 local prevPoints = table.create(numPoints, 0)
-points[1] = geo.point.new(60, -120)
+local startPoint = geo.point.new(60, 0)
+points[1] = startPoint
 prevPoints[1] = points[1]:copy()
 for i = 2, numPoints, 1 do
-	points[i] = geo.point.new(60, i - 1 + ((i - 1) * segLength) - 120)
-	prevPoints[i] = geo.point.new(60, i - 1 + ((i - 1) * segLength) - 120)
+	points[i] = geo.point.new(startPoint.x, startPoint.y + i)
+	prevPoints[i] = points[i]:copy()
 end
-local grav = geo.vector2D.new(0, 64)
 local speed = 5
-local tickTime = 0.033
+local grav = geo.vector2D.new(0, 9.8)
 
+local constraintIterations = 5
+local tickTime = 1/pd.display.getRefreshRate()
+pd.setGCScaling(0, 0)
+collectgarbage("generational")
 
 -- Updates all points according to the movement of the first point. Should be called once per frame, after any updates to the first point's location.
 function ropeSim()
 	for i = 2, #points, 1 do
+		-- calculate velocity using previous point
 		local vel = points[i] - prevPoints[i]
-		prevPoints[i] = points[i]:copy()
+
+		-- update previous point to current values (NOTE: this is setting the values not copying the points, which saves us some gc time)
+		prevPoints[i].x = points[i].x
+		prevPoints[i].y = points[i].y
+
+		-- apply velocity and gravity to each point
 		points[i] += vel
 		points[i] += grav * tickTime
 	end
@@ -49,6 +50,7 @@ end
 -- Adjusts the locations of all the points with respect to their neighbors. Should be called at least once per update cycle. Calling it more than that will increase the accuracy of the simulation but decrease performance.
 function applyConstraints()
 	for i = 1, #points - 1, 1 do
+		-- TODO: There is a fair amount of garbage being generated here, future me might want to optimize for that a bit later. When/if I get around to that, I think it'd be worth looking into storing the points as LineSegments instead of points.
 		local lineVec = points[i] - points[i + 1]
 		local dist = lineVec:magnitude()
 		local error = math.abs(dist - segLength)
@@ -70,17 +72,15 @@ function applyConstraints()
 	end
 end
 
--- Returns the length of the chain
-function chainLength()
-	return #points * segLength
-end
-
 function drawChain()
 	gfx.clear()
 	gfx.setLineWidth(3)
 	gfx.setColor(gfx.kColorBlack)
 	for i = 1, #points - 1, 1 do
-		gfx.drawLine(points[i] .. points[i + 1])
+		if (points[i + 1].y >= -segLength or points[i + 1].y >= -segLength) and
+			((points[i].x >= -8 or points[i + 1].x >= -8) and (points[i].x <= 408 or points[i + 1].x <= 408)) then
+			gfx.drawLine(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y)
+		end
 	end
 end
 
@@ -95,21 +95,18 @@ function pd.update()
 
 	ropeSim()
 
-	for n = 1, 2, 1 do
+	for n = 1, constraintIterations, 1 do
 		applyConstraints()
 	end
 
 	drawChain()
+	pd.drawFPS()
 end
 
 pd.inputHandlers.push({
 	cranked = function(change, acceleratedChange)
 		local delta = -acceleratedChange / 360 * speed * 5
 		points[1].y += delta
-		points[1].y = clamp(points[1].y, -chainLength() + 16, -1)
+		points[1].y = clamp(points[1].y, -chainLength, chainLength / 2)
 	end
 })
-
--- function setShakeAmount(amount)
--- 	screenShakeSprite:setShakeAmount(amount)
--- end
